8d666122072404466699c9c936f7395f8657e2e6
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-03-05 20:10:31 +0000

    Version 1.0.3

--- control/webpack.config.js
+++ control/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 /** @type {webpack.Configuration} */
 const config = {
     devtool: 'source-map',
+    target: ['web', 'es5'], // Keep it working in IE
 }
 
 module.exports = config
